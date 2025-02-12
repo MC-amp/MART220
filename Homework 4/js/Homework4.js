6436ac6c9f25746e971641fb598d171e777a49ac
MC-amp <matt77camp@gmail.com>
2025-02-12 15:13:42 -0700

    Homework4

---
+++ Homework 4/js/Homework4.js
@@ -0,0 +1,24 @@
+var animation = [];
+var i = 0;
+var myIdle;
+function preload(){
+    for(var i = 0; i< 10; i++){
+        myIdle = new idleAnimation("../images/Idle__00" + i + ".png");  
+        animation.push(myIdle);
+    }
+    }
+function setup(){
+    createCanvas(800, 800);
+    setInterval(updateIndexIdle, 70);
+}
+function draw(){
+    background(240);
+    animation[i].draw();
+}
+function updateIndexIdle(){
+    i+=1;
+    if(i > 9){
+        i = 0;
+    }
+    
+}
---
+++ Homework 4/js/idleAnimation.js
@@ -0,0 +1,10 @@
+class idleAnimation{
+    constructor(path) {
+        this.path = path;
+        this.myImage = loadImage(this.path);
+    }
+
+    draw() {
+        image(this.myImage, 100,100);
+    }
+}
